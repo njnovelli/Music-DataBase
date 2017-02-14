dc56100a80bbdf98db107b922b09a7b06250702a
NIC NOVELLI <NJ.NOVELLI@GMAIL.OM>
2017-02-14 13:34:16 -0500

    first commit

---
+++ db/migrate/20161010123422_remove_instrument_from_artists.rb
@@ -0,0 +1,4 @@
+class RemoveInstrumentFromArtists < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
---
+++ db/migrate/20161010124423_add_genre_ref_to_artists.rb
@@ -0,0 +1,5 @@
+class AddGenreRefToArtists < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :artists, :genre, foreign_key: true
+  end
+end
